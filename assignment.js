3632acea8a475aa17488a42325070c7089ba0d49
Nonnielle <ramzelelendu.ce@gmail.com>
2025-02-22 18:17:52 +0100

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,174 @@
+function calculateArea(length,width){return length*width}
+undefined
+console.log(cal
+VM362:1 Uncaught SyntaxError: missing ) after argument list
+console.log(calculateArea(7*4))
+VM429:1 NaN
+undefined
+console.log(calculateArea(7'4))
+VM437:1 Uncaught SyntaxError: missing ) after argument list
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+Chrome is moving towards a new experience that allows users to choose to browse without third-party cookies.
+console.log (calculateArea(7,4))
+VM515:1 28
+undefined
+console.log (calculateArea(9,6))
+VM551:1 54
+undefined
+console.log (calculateArea(8,3))
+VM569:1 24
+undefined
+function greet(name){return'Hello, ${name}!';}
+undefined
+console.log(greet("Nonnielle"))
+VM836:1 Hello, ${name}!
+undefined
+function greet(name){return "HELLO"+ name;}
+undefined
+console.log(greet("Nonnielle"))
+VM895:1 HELLONonnielle
+undefined
+function greet(name){return "Hello" + name;}
+undefined
+console.log(greet("Nonnielle"))
+VM1076:1 HelloNonnielle
+undefined
+console.log(greet("grace"))
+VM1094:1 Hellograce
+undefined
+console.log(greet("mark"))
+VM1109:1 Hellomark
+undefined
+function sumNumbers(numbers){let total = 0; for(let num of numbers){total += num;}return total;}
+undefined
+console log (sumNumbers([5,8,2]))
+VM1557:1 Uncaught SyntaxError: Unexpected identifier 'log'
+console.log (sumNumbers([5,8,2]))
+VM1568:1 15
+undefined
+console.log (sumNumbers([2,9,5]))
+VM1580:1 16
+undefined
+for (let i = 1; i <= 10; i++){console.log(i);}
+VM1807:1 1
+VM1807:1 2
+VM1807:1 3
+VM1807:1 4
+VM1807:1 5
+VM1807:1 6
+VM1807:1 7
+VM1807:1 8
+VM1807:1 9
+VM1807:1 10
+undefined
+let num = 1; while (num <= 10) {console.log(num);num++;}
+VM1975:1 1
+VM1975:1 2
+VM1975:1 3
+VM1975:1 4
+VM1975:1 5
+VM1975:1 6
+VM1975:1 7
+VM1975:1 8
+VM1975:1 9
+VM1975:1 10
+10
+function sumFor(numbers){let sum = o; for(let i = 0; i < numbers.length; i++){sum += numbers[i];}return sum;}
+undefined
+console.log (sumFor([6,7,5]))
+VM2182:1 Uncaught ReferenceError: o is not defined
+    at sumFor (<anonymous>:1:36)
+    at <anonymous>:1:14
+sumFor @ VM2182:1
+(anonymous) @ VM2251:1
+console.log(sumFor([6,7,5]))
+VM2182:1 Uncaught ReferenceError: o is not defined
+    at sumFor (<anonymous>:1:36)
+    at <anonymous>:1:13
+sumFor @ VM2182:1
+(anonymous) @ VM2255:1
+function sumFor(numbers){let sum = 0; for(let i = 0; i < numbers.length; i++){sum += numbers[i];}return sum;}
+undefined
+console.log(sumFor([6,7,5]))
+VM2272:1 18
+undefined
+console.log(sumFor([2,2,7]))
+VM2284:1 11
+undefined
+function sumEhile(numbers){let sum = 0; for(let i = 0; while ( i < numbers.length;){sum += numbers[i]; i++}return sum;}
+VM2412:1 Uncaught SyntaxError: Unexpected token 'while'
+function sumWhile(numbers){let sum = 0; let i = 0; while ( i < numbers.length;){sum += numbers[i]; i++}return sum;}
+VM2421:1 Uncaught SyntaxError: Unexpected token ';'
+function sumWhile(numbers){let sum = 0; let i = 0; while ( i < numbers.length){sum += numbers[i]; i++}return sum;}
+undefined
+console.log (sumWhile([7,4,9])
+VM2543:1 Uncaught SyntaxError: missing ) after argument list
+console.log (sumWhile([7,4,9]))
+VM2551:1 20
+undefined
+console.log (sumWhile([3,9,6]))
+VM2563:1 18
+undefined
+const color = ["blue","gold","pink","yellow","black"]; for (let i = 0; i < color.length; i++){console.log(color[i]);} 
+VM2864:1 blue
+VM2864:1 gold
+VM2864:1 pink
+VM2864:1 yellow
+VM2864:1 black
+undefined
+const names = ["grace","gold","adam","Nonnie","rema"];let i = 0; while (i < names.length){console.log(names[i]);i++} 
+VM2988:1 grace
+VM2988:1 gold
+VM2988:1 adam
+VM2988:1 Nonnie
+VM2988:1 rema
+4
+function checkEvenorOdd(num){if(num % 2 === 0){return "Even";}else{return "odd";}}
+undefined
+console.log (checkEvenorOdd(246))
+VM3481:1 Even
+undefined
+console.log (checkEvenorOdd(684))
+VM3499:1 Even
+undefined
+console.log (checkEvenorOdd(57))
+VM3528:1 odd
+undefined
+function gradeCalculator(score){if(score >= 80){return 'A';}else if (score >= 65){return 'B';}else if (score >= 50){return 'C';}else if(score >= 30){return 'D';}else{return 'F;}}
+VM4053:1 Uncaught SyntaxError: Invalid or unexpected token
+function gradeCalculator(score){if(score >= 80){return 'A';}else if (score >= 65){return 'B';}else if (score >= 50){return 'C';}else if(score >= 30){return 'D';}else{return 'F';}}
+undefined
+console.log(gradeCalculator(40))
+VM4189:1 D
+undefined
+console.log(gradeCalculator(98))
+VM4203:1 A
+undefined
+console.log(gradeCalculator(20))
+VM4217:1 F
+undefined
